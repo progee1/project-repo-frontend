242b0f78714f8ea78642f09d1d1acab2f761c7c4
progee1 <onyedika198@gmail.com>
2025-05-19 23:44:53 +0100

    Create main.jsx
    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BitcoinInvestmentApp from './BitcoinInvestmentApp';
+import './index.css'; // Optional styling
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BitcoinInvestmentApp />
+  </React.StrictMode>
+);
