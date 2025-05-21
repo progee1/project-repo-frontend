e21a1ad301fbcdd65375c41bd5adcebaa8393e6e
progee1 <onyedika198@gmail.com>
2025-05-21 22:14:18 +0100

    Update BitcoinInvestmentApp.jsx

--- src/BitcoinInvestmentApp.jsx
+++ src/BitcoinInvestmentApp.jsx
@@ -25,21 +25,12 @@ const BitcoinInvestmentApp = () => {
   }, []);
 
   useEffect(() => {
-    const addGoogleTranslateScript = () => {
-      const script = document.createElement('script');
-      script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-      script.async = true;
-      document.body.appendChild(script);
-    };
+    const script = document.createElement('script');
+    script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    script.async = true;
+    document.body.appendChild(script);
 
-    window.googleTranslateElementInit = () => {
-      new window.google.translate.TranslateElement(
-        { pageLanguage: 'en' },
-        'google_translate_element'
-      );
-    };
-
-    addGoogleTranslateScript();
+    window.googleTranslateElementInit = googleTranslateElementInit;
   }, []);
 
   const handleCalculate = () => {
