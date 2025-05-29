c0a8295ee01af50cf8e3c07b37492d5a08ca25e1
progee1 <onyedika198@gmail.com>
2025-05-29 22:07:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import AboutCompany from './components/AboutCompany';
+import WhatWeDo from './components/WhatWeDo';
+import TradingViewChart from './components/TradingViewChart';
+import CompanyContact from './components/CompanyContact';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Your React App is working!</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>Bitcoin Investment App</h1>
+      </header>
+      <main>
+        <AboutCompany />
+        <WhatWeDo />
+        <TradingViewChart />
+        <CompanyContact />
+      </main>
+      <footer>
+        <p>© 2025 Bitcoin Investment App. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
